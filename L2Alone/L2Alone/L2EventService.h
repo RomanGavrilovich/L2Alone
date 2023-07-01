cc63a547ec7b05041bf7281ca975406104e556a2
romang@playtika.com <romang@playtika.com>
2023-07-01 18:15:04 +0200

    Add logging of foreground process id

--- L2Alone/L2Alone/L2EventService.h
+++ L2Alone/L2Alone/L2EventService.h
@@ -518,6 +518,12 @@ bool L2EventService::publishKeyboard(KBDLLHOOKSTRUCT* kbdll, bool keyDown) {
 	// TODO: Remove
 	if (isKeyPressed(VK_ESCAPE)) {
 		logger.log("Press escape with active process: ", dwActiveProcess);
+
+		HWND foregroundWindow = GetForegroundWindow();
+		DWORD foregroundProcessId;
+		GetWindowThreadProcessId(foregroundWindow, &foregroundProcessId);
+
+		logger.log("Foreground process id: ", foregroundProcessId);
 	}
 
 	bool propagate = true;
